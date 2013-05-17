8583beaf55335f5f87bc8a09c95c6ac6b8a3c202
Zac Fowler <zfowler@unomaha.edu>
2013-05-17 11:29:07 -0500

    Updated GIT Ignore to ignore .idea folder

---
+++ CalculatorTest.php
@@ -0,0 +1,12 @@
+<?php
+/**
+ * Created by JetBrains PhpStorm.
+ * User: zfowler
+ * Date: 5/17/13
+ * Time: 11:25 AM
+ * To change this template use File | Settings | File Templates.
+ */
+
+class CalculatorTest extends PHPUnit_Framework_TestCase {
+
+}
