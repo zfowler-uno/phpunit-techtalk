babde9e38e13d371667f7369ae2bdfa001373a5f
Zac Fowler <zfowler@unomaha.edu>
2013-05-17 11:55:09 -0500

    Methods Created in Calculator Class, but asserts fail

--- Calculator.php
+++ Calculator.php
@@ -2,4 +2,19 @@
 
 class Calculator {
     // We will build this as we write tests.
+    public function add($a, $b) {
+
+    }
+
+    public function subtract($a, $b) {
+
+    }
+
+    public function divide($a, $b) {
+
+    }
+
+    public function multiply($a, $b) {
+        
+    }
 }
